c7dd77d33949a724351063e626d586c3c31aa6d7
Alexis Sartre <alexis.sartre@example.com>
2026-02-07 20:36:32 +0100

    Fix private annotate props

--- app/(protected)/troupes/[troupeId]/plays/[playId]/private-annotate/page.tsx
+++ app/(protected)/troupes/[troupeId]/plays/[playId]/private-annotate/page.tsx
@@ -30,7 +30,6 @@ export default async function PrivateAnnotatePage({
     return (
         <PrivateAnnotatorClient
             play={play}
-            troupeId={troupeId}
             script={script}
             privateNotes={privateNotes}
         />
